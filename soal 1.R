6327becc2a25e90a8d5c5aff0c94c80670f52d51
AfiraRolobessy03 <81508591+AfiraRolobessy03@users.noreply.github.com>
2022-04-10 19:31:37 +0700

    Add files via upload

---
+++ soal 1.R
@@ -0,0 +1,21 @@
+#Soal 1
+#A
+survei = 0.20
+n = 3
+dgeom(x = n, prob = survei)
+
+#B
+mean(rgeom(n = 10000, prob = survei) == 3)
+
+#c
+
+#D
+hist(rgeom(n,survei),main="Histogram of Geometric",xlab="n")
+
+#E
+survei = 0.2
+mean = 1/survei
+mean
+varian = (1-survei)/survei^2
+varian
+
